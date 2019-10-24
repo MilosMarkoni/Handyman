166e7c25e74ab0395b1545c2af867d60580b4c62
MilosMarkoni <markovicm04@gmail.com>
2019-10-24 17:09:15 +0200

    Add routing module

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-import './App.css';
 
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import './App.css';
 
 import Header from './components/containers/Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Header />
-
-          <Switch>
-            <Route path="/about"></Route>
-            <Route path="/users"></Route>
-            <Route path="/"></Route>
-          </Switch>
-        </Router>
+      <div>
+        <Header></Header>
       </div>
     );
   }
--- src/components/containers/Header/Header.js
+++ src/components/containers/Header/Header.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './Header.css';
 
 import { Panel } from 'primereact/panel';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 
+import MyProfile from '../MyProfile/MyProfile';
+import Order from '../Order/Order';
+import Pricing from '../Pricing/Pricing';
+
 class Header extends Component {
   render() {
     return (
-      <div className="content-section implementation">
+      <Router className="content-section implementation">
         <Panel>
           <nav>
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>
+                <Link to="/">My profile</Link>
+              </li>
+              <li>
+                <Link to="/order">Order</Link>
+              </li>
+              <li>
+                <Link to="/pricing">Pricing</Link>
+              </li>
             </ul>
           </nav>
 
           <InputText placeholder="Search" type="text" />
           <Button label="Logout" icon="pi pi-power-off" style={{ marginLeft: 4 }} />
         </Panel>
-      </div>
+
+        <Switch>
+          <Route exact path="/">
+            <MyProfile />
+          </Route>
+          <Route path="/order">
+            <Order />
+          </Route>
+          <Route path="/pricing">
+            <Pricing />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
