5325147f90b7a414d3809a44a2c8df73de5ff2d3
MilosMarkoni <markovicm04@gmail.com>
2019-10-29 15:34:28 +0100

    Add redirection to specific page after login

--- src/components/UnauthenticatedRoute/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute/UnauthenticatedRoute.js
@@ -2,12 +2,38 @@ import React from 'react';
 
 import { Route, Redirect } from 'react-router-dom';
 
+const queryString = (name, url = window.location.href) => {
+  name = name.replace(/[[]]/g, '\\$&');
+
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)', 'i');
+  const results = regex.exec(url);
+
+  if (!results) {
+    return null;
+  }
+  if (!results[2]) {
+    return '';
+  }
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
+
 export default function UnauthenticatedRoute({ component: C, appProps, ...rest }) {
+  const redirect = queryString('redirect');
+
+  console.log(redirect);
+
+  console.log(appProps.isAuthenticated);
+
   return (
     <Route
       {...rest}
       render={props =>
-        !appProps.isAuthenticated ? <C {...props} {...appProps} /> : <Redirect to="/" />
+        !appProps.isAuthenticated ? (
+          <C {...props} {...appProps} />
+        ) : (
+          <Redirect to={redirect === '' || redirect === null ? '/s' : redirect} />
+        )
       }
     />
   );
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -24,8 +24,6 @@ export default function Login(props) {
     try {
       await Auth.signIn(fields.email, fields.password);
       props.userHasAuthenticated(true);
-
-      props.history.push('/');
     } catch (error) {
       console.info(error);
       setIsLoading(false);
