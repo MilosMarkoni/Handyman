0075b2e23fd996bca58bb2181efb6ada6e015952
MilosMarkoni <markovicm04@gmail.com>
2019-11-09 11:33:36 +0100

    Working on Login screen, fixed responsive issue

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -34,46 +34,46 @@ class Header extends Component {
           <div
             className={` ui-header-panel  ${this.state.collapsed ? 'collapse' : 'not-collapsed'}`}
           >
-            <div className="logo-wrapper">
-              <img className="logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="img" />
-              {/* <div className="headerText">CompanyName</div> */}
-
-              {this.state.collapsed ? (
-                <svg
-                  onClick={() => collapseMenu()}
-                  className="collapse-menu"
-                  width="14"
-                  height="14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M12.778 11.364a1 1 0 01-1.414 1.414L6.536 7.95l-4.829 4.828a1 1 0 01-1.414-1.414L5.12 6.536.293 1.707A1 1 0 011.707.293L6.536 5.12 11.364.293a1 1 0 111.414 1.414L7.95 6.536l4.828 4.828z"
-                    fill="#A0AEC0"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  onClick={() => collapseMenu()}
-                  className="collapse-menu"
-                  width="18"
-                  height="14"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M0 1a1 1 0 011-1h16a1 1 0 110 2H1a1 1 0 01-1-1zm0 6a1 1 0 011-1h16a1 1 0 110 2H1a1 1 0 01-1-1zm1 5a1 1 0 100 2h16a1 1 0 100-2H1z"
-                    fill="#A0AEC0"
-                  />
-                </svg>
-              )}
-            </div>
-
             {isAuthenticated ? (
               <>
+                <div className="logo-wrapper">
+                  <img className="logo" src={process.env.PUBLIC_URL + '/logo.png'} alt="img" />
+                  <div className="headerText">CompanyName</div>
+
+                  {this.state.collapsed ? (
+                    <svg
+                      onClick={() => collapseMenu()}
+                      className="collapse-menu"
+                      width="14"
+                      height="14"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12.778 11.364a1 1 0 01-1.414 1.414L6.536 7.95l-4.829 4.828a1 1 0 01-1.414-1.414L5.12 6.536.293 1.707A1 1 0 011.707.293L6.536 5.12 11.364.293a1 1 0 111.414 1.414L7.95 6.536l4.828 4.828z"
+                        fill="#A0AEC0"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      onClick={() => collapseMenu()}
+                      className="collapse-menu"
+                      width="18"
+                      height="14"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M0 1a1 1 0 011-1h16a1 1 0 110 2H1a1 1 0 01-1-1zm0 6a1 1 0 011-1h16a1 1 0 110 2H1a1 1 0 01-1-1zm1 5a1 1 0 100 2h16a1 1 0 100-2H1z"
+                        fill="#A0AEC0"
+                      />
+                    </svg>
+                  )}
+                </div>
+
                 <nav className="main-navigation">
                   <ul>
                     <li>
@@ -130,18 +130,7 @@ class Header extends Component {
                 </nav>
               </>
             ) : (
-              <>
-                <nav>
-                  <ul>
-                    <li>
-                      <Link to="/signup">Sign up</Link>
-                    </li>
-                    <li>
-                      <Link to="/login">Login</Link>
-                    </li>
-                  </ul>
-                </nav>
-              </>
+              <></>
             )}
             <p className="clear"></p>
           </div>
